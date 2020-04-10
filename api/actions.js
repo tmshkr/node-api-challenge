46edeb2ee32a6b35c4d143c13dd6d6dd09636c99
Tim Shaker <tim@tmshkr.com>
2020-04-10 09:02:36 -0700

    GET /api/actions

--- api/actions.js
+++ api/actions.js
@@ -2,8 +2,13 @@ const express = require("express");
 const Actions = require("../data/helpers/actionModel");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ api: "/api/actions" });
+router.get("/", (req, res, next) => {
+  Actions.get()
+    .then((actions) => res.json(actions))
+    .catch((err) => {
+      console.error(err);
+      next({ code: 500, msg: "There was a problem retrieving the actions" });
+    });
 });
 
 module.exports = router;
