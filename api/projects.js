bb65dfb204de0e4b6fcbd33219500357ee054fff
Tim Shaker <tim@tmshkr.com>
2020-04-10 08:48:53 -0700

    GET /api/projects/:id

--- api/projects.js
+++ api/projects.js
@@ -11,4 +11,18 @@ router.get("/", (req, res, next) => {
     });
 });
 
+router.get("/:id", validateProjectID, (req, res) => res.json(req.project));
+
 module.exports = router;
+
+async function validateProjectID(req, res, next) {
+  try {
+    const project = await Projects.get(req.params.id);
+    if (!project) return next({ code: 404, msg: "Project not found" });
+    req.project = project;
+    next();
+  } catch (err) {
+    console.error(err);
+    next({ code: 500, msg: "There was a problem finding that project" });
+  }
+}
