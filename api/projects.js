098ac67d009b1149d1628fdd86293eeea6280e89
Tim Shaker <tim@tmshkr.com>
2020-04-10 08:19:34 -0700

    GET /api/projects

--- api/projects.js
+++ api/projects.js
@@ -2,8 +2,13 @@ const express = require("express");
 const Projects = require("../data/helpers/projectModel");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ api: "/api/projects" });
+router.get("/", (req, res, next) => {
+  Projects.get()
+    .then((projects) => res.json(projects))
+    .catch((err) => {
+      console.error(err);
+      next({ code: 500, msg: "There was a problem retrieving the projects" });
+    });
 });
 
 module.exports = router;
