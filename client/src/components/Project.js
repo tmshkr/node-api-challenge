17c508dbe8b648fa958521f7092200eced87119c
Tim Shaker <tim@tmshkr.com>
2020-04-10 10:04:48 -0700

    deleteProject

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import { Card, CardText, CardBody, Button } from "reactstrap";
 import axios from "../utils/axios";
 import "./Project.scss";
 
 function Project(props) {
   const { id, description, name } = props.project;
-  const deleteProject = (id) => {
+  const deleteProject = (e, id) => {
+    e.stopPropagation();
     axios.delete(`/api/projects/${id}`).then(() => props.getProjects());
   };
   return (
-    <Card className="project">
+    <Card
+      className="project"
+      onClick={() => props.history.push(`/projects/${id}`)}
+    >
       <CardBody>
         <h2>{name}</h2>
         <CardText>{description}</CardText>
-        <Button color="danger" onClick={() => deleteProject(id)}>
+        <Button color="danger" onClick={(e) => deleteProject(e, id)}>
           Delete
         </Button>
       </CardBody>
--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import Project from "./Project";
 
 function ProjectList(props) {
-  const { projects } = props;
+  const { projects, history, getProjects } = props;
   return (
     <div className="project-list">
       {projects.map((p) => (
-        <Project key={p.id} project={p} />
+        <Project
+          key={p.id}
+          project={p}
+          history={history}
+          getProjects={getProjects}
+        />
       ))}
     </div>
   );
